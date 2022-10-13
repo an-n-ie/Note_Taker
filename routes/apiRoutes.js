18b7dc6ed59719e8f8e76af2f4972f4abce463fa
Annie Hoang <anniehoang.anh@gmail.com>
2022-10-13 17:05:13 -0400

    post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
-const router=require("express").Router()
-const fs=require("fs")
-const db=require("../db/db.json")
+const router = require("express").Router()
+const fs = require("fs")
+const db = require("../db/db.json")
 
 router.get("/api/notes", (req,res)=>{
     res.json(db)
@@ -8,26 +8,20 @@ router.get("/api/notes", (req,res)=>{
 // create POST
 
 router.post("/api/notes",(req,res)=>{
-    // res.json(db)
+
     console.log(req.body);
- 
+
     const { title, text } = req.body;
-  
-    if (req.body) {
+
+    // If all the required properties are present
+    if (title && text ) {
+      // Variable for the object we will save
       const newNote = {
         title,
         text,
       };
-  
-      readAndAppend(newNote, './db/db.json');
-    } else {
-      res.error('Error in adding tip');
-    }
 
-    // db is an array; need array function to add new element or value of an array
-// think about updating the revides db with new data itno the db.json
-})
-
+ }})
 // router.delete(`/api/notes/${id}`,(req,res)=>{
 
 // })
